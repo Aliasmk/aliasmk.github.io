01faca1b3c64972bec514a40fa09af0de81580e4
Michael K <michael.kafarowski@gmail.com>
2017-02-01 21:32:12 -0500

    Added Gtrack code

--- scripts/title.js
+++ scripts/title.js
@@ -1,3 +1,13 @@
 var iam = ["Human Real Time Operating System","Techno Font Enthusiast","Safety Glasses Advocate", "Homefries and Breakfast Cereal Chef", "Graphics Card Arsonist (Unintentional)", "Pacman Cloner", "Emulator Developer", "Maya 8.0 License Holder","カナダ人です","Proud Android Owner","Virtual Reality Enthusiast"]
 var index = Math.floor(Math.random() * (iam.length));
 document.write("Mechatronics Engineering Student and " + iam[index]);
+
+//google tracking code
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-68321101-2', 'auto');
+ga('send', 'pageview');
+
