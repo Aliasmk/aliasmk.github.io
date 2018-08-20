51a83c2d01b0c2cb17ea3a50dae8a49f5bc4469f
Michael K <Aliasmk@users.noreply.github.com>
2018-08-20 09:13:55 -0400

    Add titles

--- scripts/title.js
+++ scripts/title.js
@@ -1,4 +1,4 @@
-var iam = ["Human Real Time Operating System","Techno Font Enthusiast","Safety Glasses Advocate", "Homefries and Breakfast Cereal Chef", "Graphics Card Arsonist (Unintentional)", "Pacman Cloner", "Emulator Developer", "Maya 8.0 License Holder","カナダ人です","Proud Android Owner","Virtual Reality Enthusiast"]
+var iam = ["Techno Font Enthusiast","Safety Glasses Advocate", "Homefries and Breakfast Cereal Chef", "Graphics Card Arsonist (Unintentional)", "Pacman Cloner", "Emulator Developer", "FPV Drone Pilot", "Mini Pumpkin Farmer", "Hot Sauce Masochist", "Maya 8.0 License Holder","カナダ人です","Proud Android Owner","Virtual Reality Enthusiast","Maker", "Fan of RGB LEDs", "日本語の学生です"]
 var index = Math.floor(Math.random() * (iam.length));
 document.write("Mechatronics Engineering Student and " + iam[index]);
 
