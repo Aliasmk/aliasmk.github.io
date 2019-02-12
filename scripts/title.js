ad8f44edaa4a3bc59945dc1f7c6f2a8098ae7b29
Michael K <Aliasmk@users.noreply.github.com>
2019-02-12 09:47:49 -0500

    Update title.js

--- scripts/title.js
+++ scripts/title.js
@@ -1,13 +1,3 @@
 var iam = ["Techno Font Enthusiast","Safety Glasses Advocate", "Homefries and Breakfast Cereal Chef", "Graphics Card Arsonist (Unintentional)", "Pacman Cloner", "Emulator Developer", "FPV Drone Pilot", "Mini Pumpkin Farmer", "Hot Sauce Masochist", "Maya 8.0 License Holder","カナダ人です","Proud Android Owner","Virtual Reality Enthusiast","Maker", "Fan of RGB LEDs", "日本語の学生です"]
 var index = Math.floor(Math.random() * (iam.length));
 document.write("Mechatronics Engineering Student and " + iam[index]);
-
-//google tracking code
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-68321101-2', 'auto');
-ga('send', 'pageview');
-
