0132df481288a2bdb80b403a81571a27ef9572f3
Michael Kafarowski <michael.kafarowski@gmail.com>
2019-02-20 08:17:34 -0500

    removed some welcome messages, moved around old images to clean up

--- scripts/title.js
+++ scripts/title.js
@@ -1,3 +1,3 @@
-var iam = ["Techno Font Enthusiast","Safety Glasses Advocate", "Homefries and Breakfast Cereal Chef", "Graphics Card Arsonist (Unintentional)", "Pacman Cloner", "Emulator Developer", "FPV Drone Pilot", "Mini Pumpkin Farmer", "Hot Sauce Masochist", "Maya 8.0 License Holder","カナダ人です","Proud Android Owner","Virtual Reality Enthusiast","Maker", "Fan of RGB LEDs", "日本語の学生です"]
+var iam = ["Techno Font Enthusiast","Safety Glasses Advocate", "Pacman Cloner", "Emulator Developer", "FPV Drone Pilot", "Mini Pumpkin Farmer", "Hot Sauce Masochist", "Maya 8.0 License Holder","カナダ人です","Proud Android Owner","Virtual Reality Enthusiast","Maker", "Fan of RGB LEDs", "日本語の学生です"]
 var index = Math.floor(Math.random() * (iam.length));
 document.write("Mechatronics Engineering Student and " + iam[index]);
