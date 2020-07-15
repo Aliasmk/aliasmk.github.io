edcb3797099fcf2c1b03d87a21145704d08950bf
Michael Kafarowski <michael.kafarowski@gmail.com>
2020-07-14 22:15:55 -0400

    Spelling

--- scripts/title.js
+++ scripts/title.js
@@ -1,4 +1,4 @@
-var iam = ["Techno Font Enthusiast","Safety Glasses Advocate", "Pacman Cloner", "Emulator Developer", "FPV Drone Pilot", "Mini Pumpkin Farmer", "Hot Sauce Masochist", "Maya 8.0 License Holder","カナダ人です","Proud Android Owner","Virtual Reality Enthusiast","Maker", "Fan of RGB LEDs", "日本語の学生です", "Moka Pot Cappuccino Maker", "Enthusiastic Learner", "Electron Tamer", "Dabbler of Different Disciplines"]
+var iam = ["Techno Font Enthusiast","Safety Glasses Advocate", "Pacman Cloner", "Emulator Developer", "FPV Drone Pilot", "Mini Pumpkin Farmer", "Hot Sauce Masochist", "Maya 8.0 License Holder", "カナダ人です", "Proud Android Owner", "Virtual Reality Enthusiast", "Maker", "Fan of RGB LEDs", "日本語の学生です", "Moka Pot Cappuccino Maker", "Enthusiastic Learner", "Electron Tamer", "Dabbler of Different Disciplines", "Embedded Developer", "Fan of OLED displays", "Laser Systems Enthusiast"];
 var index = Math.floor(Math.random() * (iam.length));
 var anda = " and " + iam[index];
 document.getElementById("nameSubscript").innerHTML += "Mechatronics Engineering Graduate";
